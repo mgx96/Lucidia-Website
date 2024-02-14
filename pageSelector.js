a1a4abac9eee2baf016d6cffe1c903f64853d07f
progspace1204 <progspace1204@gmail.com>
2024-02-14 12:34:23 -0800

    design: base structure for games

--- pageSelector.js
+++
@@ -1,9 +0,0 @@
-
-let navButtons = document.querySelectorAll(".navbuttons a");
-let bodyId = document.querySelector("body").id;
-
-for(let navButton of navButtons){
-    if(navButton.dataset.active == bodyId){
-        navButton.classList.add("active");
-    }
-}
