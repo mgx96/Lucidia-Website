5e56c2e45bfe386085cc14d63aa4193833da7721
Malek <m.malek1996@hotmail.com>
2024-02-14 21:07:11 +0100

    Major Updates

--- hamMenu.js
+++ hamMenu.js
@@ -1,6 +1,7 @@
 var topLine = document.querySelector(".top");
 var midLine = document.querySelector(".middle");
 var botLine = document.querySelector(".bottom");
+var menu = document.querySelector(".menu");
 
 randomNumber = Math.random();
 randomNumber = Math.floor(randomNumber * 4) + 1;
@@ -11,5 +12,5 @@ document.querySelector('.hamburger').addEventListener('click', function(){
     topLine.classList.toggle('active');
     midLine.classList.toggle('active');
     botLine.classList.toggle('active');
-    console.log("Clicked!");
+    menu.classList.toggle('active');
 });
